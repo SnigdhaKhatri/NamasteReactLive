61eaca1eb4034ba08005968b5af0fa0cb2147e9e
snigdha khatri <snigdhakhatri97@gmail.com>
2023-01-08 00:26:49 +0530

    removed assignment code from main app.js file

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ import ReactDOM from "react-dom/client";
 
 const HeaderComponent = () => {
   return (
-    <div className="navBar">
-      <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"></img>
-      <input type="text" placeholder="Search.." />
-      <img className="user" src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"></img>
+    <div>
+      
     </div>
   );
 }
